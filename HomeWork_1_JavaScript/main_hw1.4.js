fff28bafde8bb84d90fb807aba14c5ddc45cf149
BohdanDem <bogdandemchuk.1@gmail.com>
2023-05-02 11:09:17 +0300

    init commit__HomeWork_1_JavaScript

---
+++ HomeWork_1_JavaScript/main_hw1.4.js
@@ -0,0 +1,7 @@
+let name = prompt('Enter your name')
+let surName = prompt('Enter your surname')
+let lastName = prompt('Enter your lastname')
+
+const FULL_NAME =`${surName} ${name} ${lastName}`
+
+console.log(FULL_NAME)
---
+++ HomeWork_1_JavaScript/main_hw1.js
@@ -0,0 +1,53 @@
+
+// home work 1.1
+//=========================================
+
+let greeting = 'hello'
+const SCHOOL = 'owu'
+let dom = 'com'
+const COUNTRY = 'ua'
+let num = 1
+let num2 = 10
+const X = -999
+let y = 123
+const PI = 3.14
+let d = 2.7
+let age = 16
+let status = true
+let f = false
+
+console.log(greeting)
+console.log(SCHOOL)
+console.log(dom)
+console.log(COUNTRY)
+console.log(num)
+console.log(num2)
+console.log(X)
+console.log(y)
+console.log(PI)
+console.log(d)
+console.log(age)
+console.log(status)
+console.log(f)
+
+// home work 1.2
+//=========================================
+
+let firstName = 'Bohdan'
+let middleName = 'Demchuk'
+let lastName = 'Volodymyrovych'
+
+const USER = `${middleName} ${firstName} ${lastName}`
+
+console.log(USER)
+
+// home work 1.3
+//=========================================
+
+let a = 100
+let b = '100'
+let c = true
+
+console.log(typeof a, a)
+console.log(typeof b, b)
+console.log(typeof c, c)
