ec4216db6040b4c144edf0ff06c481330c947786
BohdanDem <bogdandemchuk.1@gmail.com>
2023-06-10 12:24:05 +0300

    Added__added additional9

--- HomeWork_9_JavaScript/additional9/main_hw_add 9.3.js
+++ HomeWork_9_JavaScript/additional9/main_hw_add 9.3.js
@@ -3,13 +3,14 @@
 
 let contenteditable = document.createElement('div')
 contenteditable.classList.add('contenteditable')
-contenteditable.innerHTML = '25'
+contenteditable.innerText = 'asd'
 contenteditable.style.fontSize = '40px'
 
-contenteditable.onkeydown = function (ev) {
-    if (ev.key === 'tab') {
-        contenteditable.innerHTML = '20'
-    }
-}
-
 document.body.appendChild(contenteditable)
+
+window.onkeydown = function (ev) {
+    if (ev.key === 'Tab') {
+        contenteditable.innerHTML = `<${contenteditable.innerText}></${contenteditable.innerText}>`
+    }
+    document.body.replaceChildren(contenteditable.innerHTML)
+}
