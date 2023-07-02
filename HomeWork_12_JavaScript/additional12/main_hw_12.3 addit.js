962611f82c9196d371eaad16a9343ab2a60259fc
BohdanDem <bogdandemchuk.1@gmail.com>
2023-07-02 11:31:59 +0300

    Added__addition task HW_12_JavaScript

--- HomeWork_12_JavaScript/additional12/main_hw_12.3 addit.js
+++ HomeWork_12_JavaScript/additional12/main_hw_12.3 addit.js
@@ -9,6 +9,19 @@
 // На сторінці  list.html побудувати кнопку яка видаляє всі товари з корзини та локалстораджа.
 // До кожного товару додати кнопку, при кліку на яку з лс видаляється конкретний обраний  товар
 
+let form = document.forms['addNewProduct']
+form.onsubmit  = function (e) {
+    e.preventDefault()
+    let product = {
+        ProductName : this.productName.value,
+        ProductCount : this.productCount.value,
+        ProductPrice : this.productPrice.value,
+        ProductImage : this.productImage.value
+    }
 
+    let productsPage = JSON.parse(localStorage.getItem('productsPage')) || [];
+    productsPage.push(product);
+    localStorage.setItem('productsPage', JSON.stringify(productsPage));
+}
 
 
